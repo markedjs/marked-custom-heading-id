f622f98af12c60e1a6e864ef0e9eb5cdf533bf82
Tony Brix <tony@brix.ninja>
2023-09-03 17:55:09 -0600

    no heading id if no custom one specified for v8

--- spec/index.test.js
+++ spec/index.test.js
@@ -1,19 +1,19 @@
-import { marked } from 'marked';
+import { Marked } from 'marked';
 import cusomtHeadingId from '../src/index.js';
 
 describe('custom-heading-id', () => {
+  let marked;
   beforeEach(() => {
-    marked.setOptions(marked.getDefaults());
-    marked.use({ headerIds: true });
+    marked = new Marked();
   });
 
   test('no options', () => {
     marked.use(cusomtHeadingId());
-    expect(marked('# heading {#custom-id}')).toBe('<h1 id="custom-id">heading</h1>\n');
+    expect(marked.parse('# heading {#custom-id}')).toBe('<h1 id="custom-id">heading</h1>\n');
   });
 
   test('slug id if no custom id specified', () => {
     marked.use(cusomtHeadingId());
-    expect(marked('# heading')).toBe('<h1 id="heading">heading</h1>\n');
+    expect(marked.parse('# heading')).toBe('<h1>heading</h1>\n');
   });
 });
