e3dfd51c19b6048447ea1d1a28a9d27cb2c7d556
Tony Brix <tony@brix.ninja>
2024-06-12 00:44:13 -0600

    fix: allow marked v13

--- src/index.js
+++ src/index.js
@@ -1,14 +1,15 @@
 export default function customHeadingId() {
   return {
+    useNewRenderer: true,
     renderer: {
-      heading(text, level, raw, slugger) {
+      heading({ text, depth }) {
         const headingIdRegex = /(?: +|^)\{#([a-z][\w-]*)\}(?: +|$)/i;
         const hasId = text.match(headingIdRegex);
         if (!hasId) {
           // fallback to original heading renderer
           return false;
         }
-        return `<h${level} id="${hasId[1]}">${text.replace(headingIdRegex, '')}</h${level}>\n`;
+        return `<h${depth} id="${hasId[1]}">${text.replace(headingIdRegex, '')}</h${depth}>\n`;
       }
     }
   };
