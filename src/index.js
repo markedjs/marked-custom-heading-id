aac18e7e86cc8d1ce476eb3b9e8174923591be88
Tony Brix <tony@brix.ninja>
2025-03-11 23:52:56 -0600

    fix: fix marked <13 without new renderer (#647)
    * fix: fix marked <13 without new renderer
    
    * lint

--- src/index.js
+++ src/index.js
@@ -2,7 +2,12 @@ export default function customHeadingId() {
   return {
     useNewRenderer: true,
     renderer: {
-      heading({ text, depth }) {
+      heading(text, depth) {
+        /* istanbul ignore next */
+        if (typeof text !== 'string') {
+          depth = text.depth;
+          text = text.text;
+        }
         const headingIdRegex = /(?: +|^)\{#([a-z][\w-]*)\}(?: +|$)/i;
         const hasId = text.match(headingIdRegex);
         if (!hasId) {
