c632411e4ed1009817d7d22ee1b9cb6f0a61e260
Tony Brix <tony@brix.ninja>
2025-03-11 23:50:09 -0600

    fix: fix marked <13 without new renderer

--- src/index.js
+++ src/index.js
@@ -2,7 +2,11 @@ export default function customHeadingId() {
   return {
     useNewRenderer: true,
     renderer: {
-      heading({ text, depth }) {
+      heading(text, depth) {
+        if (typeof text !== "string") {
+          depth = text.depth;
+          text = text.text;
+        }
         const headingIdRegex = /(?: +|^)\{#([a-z][\w-]*)\}(?: +|$)/i;
         const hasId = text.match(headingIdRegex);
         if (!hasId) {
